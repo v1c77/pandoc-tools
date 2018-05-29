3b86d13f23552a395a516d44ec4ab3c2745565c3
vici <heyuhuade@gmail.com>
2018-05-29 13:08:09 +0800

    init

---
+++ spec/pandoc-tools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PandocToolsView from '../lib/pandoc-tools-view';
+
+describe('PandocToolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
